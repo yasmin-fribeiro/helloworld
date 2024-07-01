ad5397a32dcddd61a2f50d18a50e1affd2f8f16e
Yasmin Ribeiro <yasminfrancaribeiro@gmail.com>
2024-07-01 13:07:48 -0300

    Validação da Classe Controladora, utilizando a camada controller atualizada.

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping("/")
 public class HelloWorldController {
 
-	@GetMapping
+	@GetMapping ("hello-world")
 	public String helloWorld() {
 		return "Hello World!!!";
+	}
+	
+	@GetMapping ("persistencia")
+	public String bsm () {
+		return "Persistência";
+	}
+	
+	@GetMapping ("objetivos")
+	public String objetivosDaSemana() {
+		return "Revisar conteúdo, revisar e enviar currículo, reunir com o grupo de PI";
 	}
 }
